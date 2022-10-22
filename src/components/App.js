0a07a286313ea97ae98c435bd4ffc5f988b0b121
Alana Martins <alanascmartins@gmail.com>
2022-10-21 22:32:22 -0300

    apostila8(react-hooks) - fim da secao 2.7

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import Accordion from "./Accordion"
+const itens = [
+  {
+    titulo: "Java",
+    descricao: "Linguagem compilada e interpretada"
+  },
+  {
+    titulo: "Python",
+    descricao: "Linguagem interpretada e dinamicamente tipada"
+  },
+  {
+    titulo: "Javascript",
+    descricao: "Interpretada. Executa do lado do cliente e do lado do servidor também"
+  }
+]
+const App = () => {
+  return (
+    <div>
+      <Accordion itens = {itens}/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+import'./components/Accordion.css'
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
